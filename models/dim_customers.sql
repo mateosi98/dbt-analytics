37e1058f7e9b2f43a63f2da135905ab51b80cfd3
mateosi98@gmail.com <mateosi98@gmail.com>
2023-07-21 09:17:05 +0000

    Building your first model - picked up after some weeks

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,12 +1,14 @@
 with customers as (
 
-    select * from {{ ref('stg_customers') }}
+    select *
+    from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
-    select * from {{ ref('stg_orders') }}
+    select *
+    from {{ ref('stg_orders') }}
 
 ),
 
